914d7f2ad9160a859baaf9eeeee87be486f79da7
KokhanskayaKsenia <5615919@mail.ru>
2021-12-16 22:04:50 +0300

    Добавлена обработка исключений.

--- Pz_5_2/__init__.py
+++ Pz_5_2/__init__.py
@@ -3,13 +3,37 @@
 # являющиеся одновременно входными и выходными). Используя четыре вызова этой функции,
 # найти минимальное и максимальное из данных чисел A, B, C,
 print("Введите число A")
-A = int (input())
+A = input()
 print("Введите число B")
-B = int (input())
+B = input()
 print("Введите число C")
-C = int (input())
+C = input()
 print("Введите число D")
-D = int (input())
+D = input()
+while type(A) != int:  # Обработка исключений 1-го числа
+    try:
+        A = int(A)
+    except ValueError:
+        print("Не то ввели!")
+        A = input("Введите первое целое число: ")
+while type(B) != int:  # Обработка исключений 2-го числа
+    try:
+        B = int(B)
+    except ValueError:
+        print("Не то ввели!")
+        B = input("Введите второе целое число: ")
+while type(C) != int:  # Обработка исключений 3-го числа
+    try:
+        C = int(C)
+    except ValueError:
+        print("Не то ввели!")
+        C = input("Введите третье целое число: ")
+while type(D) != int:  # Обработка исключений 4-го числа
+    try:
+        D = int(D)
+    except ValueError:
+        print("Не то ввели!")
+        D = input("Введите четвертое целое число: ")
 
 def Minmax(X, Y): # функция сортировки
     if X < Y:
